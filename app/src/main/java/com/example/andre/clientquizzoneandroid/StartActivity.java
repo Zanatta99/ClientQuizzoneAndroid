bc7d9bbd80943103484a25c5b6c1464a21784064
Andrea <andrea993b@gmail.com>
2018-03-01 09:43:10 +0100

    Cambiamenti

--- app/src/main/java/com/example/andre/clientquizzoneandroid/StartActivity.java
+++ app/src/main/java/com/example/andre/clientquizzoneandroid/StartActivity.java
@@ -1,7 +1,13 @@
 package com.example.andre.clientquizzoneandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button avvio = (Button) findViewById( R.id.avvio );
+
+        avvio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartActivity.this, ConnectionActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.user_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_menu:
+                // Do whatever you want to do on logout click.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
